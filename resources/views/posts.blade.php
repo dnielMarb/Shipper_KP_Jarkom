b4fb936f54f3b0ada3e070e8a6e2f10c2508beff
dnielMarb <danielninedo@gmail.com>
2022-08-29 13:01:04 +0700

    Update.123

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,12 +4,13 @@
 
 <h1 class="mb-5">Halaman BLog Posts</h1>
 @foreach($posts as $post)
-<article>
+<article class="mb-5 border-botton pb-2">
     <h2>
-       <a href="/posts/{{ $post->slug }}"> {{ $post->title }}</a>
+       <a href="/posts/{{ $post->slug }}" class="text-decoration=none"> {{ $post->title }}</a>
     </h2>
+    <p>By. Daniel Marb in <a href="/categories/{{ $post->category->slug }} " class="text-decoration=none" >  {{ $post->category->name  }}</a> | <a href="/categories" class="text-decoration=none">all categories</a></p>
     <!-- <h5>By : {{ $post->author }}</h5> -->
-    <p>{{ $post->excerpt }}</p>
+    <p>{{ $post->excerpt }} <a href="/posts/{{ $post->slug }}" class="text-decoration=none">Read more...</a></p>
 </article>
 @endforeach
 
